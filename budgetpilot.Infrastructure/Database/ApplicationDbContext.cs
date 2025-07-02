1a12dca15ce32fc925e7f986635a18dba21b0f1c
Eldar Dautović <eldarcoox123@gmail.com>
2025-07-02 22:58:26 +0200

    init: initial commit, initial migration, create ApplicationDbContext, creating Core and Infrastructure layer.

---
+++ budgetpilot.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using budgetpilot.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budgetpilot.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=budgetpilotdb;Username=admin;Password=admin123");
+
+        public DbSet<User> Users { get; set; }
+    }
+}
